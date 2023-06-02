2f192f6e95073716c6d13c83b19d8222c287234b
niloy <n.niloy06091996@gmail.com>
2023-06-02 19:09:33 +0600

    first commit

---
+++ simpleWebForm/src/main/java/com/form/simpleWebForm/SimpleWebFormApplication.java
@@ -0,0 +1,13 @@
+package com.form.simpleWebForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebFormApplication.class, args);
+	}
+
+}
---
+++ simpleWebForm/src/test/java/com/form/simpleWebForm/SimpleWebFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.form.simpleWebForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
